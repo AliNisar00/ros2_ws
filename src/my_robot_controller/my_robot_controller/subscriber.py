606a3a5f17b9cd70bf859b2f9aefffa681aa4e2a
AliNisar00 <syed.alinisar2002@gmail.com>
2025-02-14 12:32:11 +0500

    Initialized basic ROS2 publisher-subscriber

---
+++ src/my_robot_controller/my_robot_controller/subscriber.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class RobotStatusSubscriber(Node):
+    def __init__(self):
+        super().__init__('robot_status_subscriber')
+        self.subscription = self.create_subscription(String, 'robot_status', self.callback, 10)
+        self.get_logger().info("Robot Status Subscriber Node Started")
+
+    def callback(self, msg):
+        self.get_logger().info(f'Received: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RobotStatusSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
